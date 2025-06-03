53f085c72ac00ec9adbc2bd579b900531200130a
Piotr Janusz <piotrjanuszuk@gmail.com>
2025-06-03 11:00:03 +0100

    Ball game not playable, still need to add a victory condition chcker

--- My project/Assets/Balls/Pipemanager.cs
+++ My project/Assets/Balls/Pipemanager.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class Pipemanager : MonoBehaviour
 {
+    // List storing all the holders and the balls they contain
+    // formatted as [holder, ball] with [0,0] being the bottom ball in the leftmost holder
     private string[,] mainList = new string[10, 10];
 
     public Dictionary<string, int> colorTotals;
@@ -29,9 +31,43 @@ public class Pipemanager : MonoBehaviour
     public float xSpacing;
     public float ySpacing;
 
-    public void moveBalls()
-    {
+    private int selectedHolder = -1;
 
+    public void moveBalls(int fromHolder, int toHolder)
+    {
+        // check if the fromHolder is not empty
+        if (mainList[fromHolder, 0] == "empty")
+        {
+            return;
+        }
+        // Check if toHolder is not full
+        if (mainList[toHolder, 9] != "empty")
+        {
+            return;
+        }
+
+        string ballToMove = "empty";
+
+        // Find top ball in fromHolder
+        for(int i = 9; i >= 0; i--)
+        {
+            if (mainList[fromHolder, i] != "empty")
+            {
+                ballToMove = mainList[fromHolder, i];
+                mainList[fromHolder, i] = "empty";
+                break;
+            }
+        }
+        // Find top spot in toHolder
+        for (int i = 0; i < 10; i++)
+        {
+            if (mainList[toHolder, i] == "empty")
+            {
+                mainList[toHolder,i] = ballToMove;
+                break;
+            }
+        }
+        DrawBalls();
     }
 
     private void Start()
@@ -45,6 +81,53 @@ public class Pipemanager : MonoBehaviour
         {
             setupBalls();
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+
+
+            if (hit.collider != null)
+            {
+                if(!hit.transform.gameObject.CompareTag("holder"))
+                {
+                    return;
+                }
+
+                // Get which holder was clicked
+                GameObject currentHolder = hit.transform.gameObject;
+                int clickedHolder = int.Parse(currentHolder.name.Substring(currentHolder.name.Length - 1));
+
+                // Check if a holder was not previously clicked
+                if(selectedHolder == -1)
+                {
+                    selectedHolder = clickedHolder;
+                }
+                // Otherwise one was already selected so its either a double click or a slection
+                else
+                {
+                    if(selectedHolder == clickedHolder)
+                    {
+                        return;
+                    }
+                    if (selectedHolder != clickedHolder)
+                    {
+                        print("Moving from " +  clickedHolder.ToString() + " To " + selectedHolder.ToString());
+                        // Try to move the balls
+                        moveBalls(selectedHolder, clickedHolder);
+                        // reset the selected holder to reset the interaction
+                        selectedHolder = -1;
+                    }
+                }
+
+            }
+
+
+        }
+
+
     }
 
     private void DrawBalls()
@@ -59,6 +142,7 @@ public class Pipemanager : MonoBehaviour
             GameObject holder = Instantiate(ballHolder);
             balls.Add(holder);
             holder.transform.position = new Vector3(ballHolderXStartOffset + (i * ballHolderXSpacing), ballHolderYStartOffset, 0);
+            holder.name = "Holder" + i.ToString();
             for (int j = 0; j < 10; j++)
             {
                 
